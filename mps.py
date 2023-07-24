f7456d843f83643752823f4b21c7c48ec5043ceb
Lydia-England <lydiajoyengland@gmail.com>
2023-07-24 09:36:28 -0700

    added calculate eigenvalues and eigenvectors

--- mps.py
+++ mps.py
@@ -2,7 +2,7 @@
 ### mps.py                                        ###
 ### Author:        Lydia England                  ###
 ### Creation Date: 07.21.23                       ###
-### Last Modified: 07.21.23                       ###
+### Last Modified: 07.24.23                       ###
 ### Purpose: Take in an nxn matrix T.             ###
 ###          Take sqrt(T) element-wise.           ###
 ###          Take the tensor product of resultant ###
@@ -11,6 +11,9 @@
 
 import numpy as np
 import time
+import argparse
+from   numpy.linalg   import eig
+from   terminaltables import AsciiTable
 
 # define a function to check if a matrix is invertible
 def is_invertible(a):
@@ -75,48 +78,51 @@ time.sleep(time_a)
 if input("Press q to quit. Press any other key to confirm and continue with this matrix.\n") == 'q':
     exit()
 
-row_sum = np.sum(T, axis=1)  # calculate sum of values in each row.
-for i in range(num_rows):
-    # if the values in a row do not add to 1, optionally normalize so that they do.
-    if row_sum[i] != 1:
-        print("Values in row ", str(i+1), " add to ", row_sum[i], " not 1.")
-        time.sleep(time_a)
-        print("Would you like to normalize row ", str(i+1), "?")
-        time.sleep(time_a)
-        # if user types 'y' followed by the enter key, the row will be normalized such that
-        # its sum will equal 1. If user types any other key, the row will not be normalized.
-        if input("Press y for YES. Press any other key to continue without normalizing.\n") == 'y':
-            time.sleep(time_a)
-            print("Normalizing row ", str(i+1))
-            T[i,:] = T[i,:]/row_sum[i]  # normalize row by dividing each value by sum of values.
-            time.sleep(time_a)
-            print("Row ", str(i+1), "is now ", str(T[i,:]), "\n")
 
-time.sleep(time_a)
-col_sum = np.sum(T, axis=0)  # calculate sum of values in each column.
-for j in range(num_cols):
-    # if the values in a column do not add to 1, optionally normalize so that they do.
-    if col_sum[j] != 1:
-        print("Values in column", str(j+1), " add to ", col_sum[j], " not 1.")
-        time.sleep(time_a)
-        print("Would you like to normalize column", str(j+1), "?")
-        time.sleep(time_a)
-        # if user types 'y' followed by the enter key, the column will be normalized such that
-        # its sum will equal 1. If user types any other key, the column will not be normalized.
-        if input("Press y for YES. Press any other key to continue without normalizing.\n") == 'y':
+if input("Press y to check row/column sums of matrix and optionally normalize to 1. Press any other key to skip this step.\n") == 'y':
+    row_sum = np.sum(T, axis=1)  # calculate sum of values in each row.
+    for i in range(num_rows):
+        # if the values in a row do not add to 1, optionally normalize so that they do.
+        if row_sum[i] != 1:
+            print("Values in row ", str(i+1), " add to ", row_sum[i], " not 1.")
             time.sleep(time_a)
-            print("Normalizing column", (j+1))
-            T[:,j] = T[:,j]/col_sum[j]  # normalize column by dividing each value by sum of values.
+            print("Would you like to normalize row ", str(i+1), "?")
             time.sleep(time_a)
-            print("Column ", str(j+1), "is now ", str(T[:,j]), "\n")
+            # if user types 'y' followed by the enter key, the row will be normalized such that
+            # its sum will equal 1. If user types any other key, the row will not be normalized.
+            if input("Press y for YES. Press any other key to continue without normalizing.\n") == 'y':
+                time.sleep(time_a)
+                print("Normalizing row ", str(i+1))
+                T[i,:] = T[i,:]/row_sum[i]  # normalize row by dividing each value by sum of values.
+                time.sleep(time_a)
+                print("Row ", str(i+1), "is now ", str(T[i,:]), "\n")
 
-# confirm matrix with user
-time.sleep(time_b)
-print("\nThe matrix T is: \n", T)
-time.sleep(time_a)
-if input("Press q to quit. Press any other key to confirm and continue with this matrix.\n") == 'q':
-    exit()
-time.sleep(time_a)
+    time.sleep(time_a)
+    col_sum = np.sum(T, axis=0)  # calculate sum of values in each column.
+    for j in range(num_cols):
+        # if the values in a column do not add to 1, optionally normalize so that they do.
+        if col_sum[j] != 1:
+            print("Values in column", str(j+1), " add to ", col_sum[j], " not 1.")
+            time.sleep(time_a)
+            print("Would you like to normalize column", str(j+1), "?")
+            time.sleep(time_a)
+            # if user types 'y' followed by the enter key, the column will be normalized such that
+            # its sum will equal 1. If user types any other key, the column will not be normalized.
+            if input("Press y for YES. Press any other key to continue without normalizing.\n") == 'y':
+                time.sleep(time_a)
+                print("Normalizing column", (j+1))
+                T[:,j] = T[:,j]/col_sum[j]  # normalize column by dividing each value by sum of values.
+                time.sleep(time_a)
+                print("Column ", str(j+1), "is now ", str(T[:,j]), "\n")
+
+    # confirm matrix with user
+    time.sleep(time_b)
+    print("\nThe matrix T is: \n", T)
+    time.sleep(time_a)
+    if input("Press q to quit. Press any other key to confirm and continue with this matrix.\n") == 'q':
+        exit()
+    time.sleep(time_a)
+
 
 # calculate matrix A (square root of matrix T, element-wise).
 # each element in matrix A is the square root of each element in matrix T.
@@ -133,24 +139,38 @@ print("Transpose of matrix A is:\n", At)
 time.sleep(time_a)
 
 print("\nTaking tensor (Kronecker) product of A and A^T\n")
-kronA = np.kron(A, At)  # calculate tensor product of A and A transpose
+E = np.kron(A, At)  # calculate tensor product of A and A transpose
 time.sleep(time_b)
-print("Result is: \n", np.round(kronA, decimals=decimal_places))
+print("Result is: \n", np.round(E, decimals=decimal_places))
 
 time.sleep(time_b)
 
 # check if the tensor product of A and A transpose is invertible
-if is_invertible(kronA) == True:
-    print("\nThe tensor product of A and A^T is an invertible matrix of dimension", kronA.shape)
+if is_invertible(E) == True:
+    print("\nMatrix E (tensor product of A and A^T) is an invertible matrix of dimension", E.shape)
 else:
-    print("\nThe tensor product of A and A^T is a non-invertible matrix of dimension", kronA.shape)
+    print("\nMatrix E (tensor product of A and A^T) is a non-invertible matrix of dimension", E.shape)
 
 time.sleep(time_b)
+
+# calculate eigenvalues, eigenvectors of matrix E
+print("\nCalculating eigenvalues and eigenvectors of matrix E...\n")
+evals, evecs = eig(E)
+# format eigenvalues, eigenvectors as table
+table_data = []
+table_data.append(['Eigenvalues', 'Eigenvectors'])
+for i in range(len(evals)):
+    table_data.append([evals[i], evecs[i]])
+table = AsciiTable(table_data)
+time.sleep(time_b)
+print(table.table)
+
+time.sleep(time_a)
 
 # prompt user to determine if array should be saved to a text file
 print("\nWould you like to save this matrix to a text file?\n")
 if input("Press y for YES. Press any other key to continue without saving.\n") == 'y':
-    np.savetxt('a_kron_at.txt', kronA, fmt='%.4f')
+    np.savetxt('E_matrix.txt', E, fmt='%.4f')
 
 
 
